ec97677f41fd822cb7e559efd5b3d1ff6c305bd2
douglas-vitoriano <dmv@tokenizadora.com.br>
2025-05-04 14:45:12 -0300

    Deploy inicial do site

---
+++ frontend/javascript/index.js
@@ -0,0 +1,22 @@
+import "$styles/index.css"
+import "$styles/syntax-highlighting.css"
+
+// Import all JavaScript & CSS files from src/_components
+import components from "$components/**/*.{js,jsx,js.rb,css}"
+
+console.info("Bridgetown is loaded!")
+
+document.addEventListener('DOMContentLoaded', () => {
+  const $navbarBurgers = Array.from(document.querySelectorAll('.navbar-burger'));
+
+  $navbarBurgers.forEach(el => {
+    el.addEventListener('click', () => {
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+    });
+  });
+});
+
