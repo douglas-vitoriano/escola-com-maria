fb92f055fb343587409accd8e1b272e823c75cfb
douglas-vitoriano <dmv@tokenizadora.com.br>
2025-05-05 20:43:24 -0300

    fix: update yarn.lock to sync dependencies

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,5 +1,5 @@
 // Import all JavaScript & CSS files from src/_components
 // import components from "$components/**/*.{js,jsx,js.rb,css}"
-Object.entries(images).forEach(image => image)
+
 console.info("Bridgetown is loaded!")
 
