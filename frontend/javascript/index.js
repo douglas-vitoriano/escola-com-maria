7be57db3135ef2f13c6a48e24de7ed10c442b48b
douglas-vitoriano <dmv@tokenizadora.com.br>
2025-05-05 14:04:52 -0300

    typo

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,5 +1,6 @@
 import "$styles/index.css"
 import "$styles/syntax-highlighting.css"
+import "bulma/css/bulma.css"
 
 // Import all JavaScript & CSS files from src/_components
 // import components from "$components/**/*.{js,jsx,js.rb,css}"
