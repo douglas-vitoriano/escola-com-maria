60b7d116c98f8c61b8106fa41f60a1c49be73f7e
douglas-vitoriano <dmv@tokenizadora.com.br>
2025-05-12 09:30:54 -0300

    fix: api

--- plugins/liturgia_api.rb
+++ plugins/liturgia_api.rb
@@ -1,29 +1,53 @@
 require "net/http"
 require "json"
 require "uri"
+require "fileutils"
+require "date"
 
 module LiturgiaApi
   API_URL = "https://liturgia.up.railway.app/v2/"
+  CACHE_FILE = "_cache/liturgia_data.json"
+  CACHE_EXPIRATION_DAYS = 1
 
   def self.buscar_data
-    uri = URI(API_URL)
-    response = Net::HTTP.get_response(uri)
-
-    if response.is_a?(Net::HTTPSuccess)
-      json = JSON.parse(response.body)
-      if json["erro"]
-        Bridgetown.logger.warn "Liturgia API", "Erro na resposta: #{json['erro']}"
-        return nil
-      else
-        return json
-      end
+    if cache_valido?
+      return JSON.parse(File.read(CACHE_FILE))
     else
-      Bridgetown.logger.error "Liturgia API", "Erro HTTP: #{response.code}"
-      nil
+      uri = URI(API_URL)
+      response = Net::HTTP.get_response(uri)
+
+      if response.is_a?(Net::HTTPSuccess)
+        json = JSON.parse(response.body)
+        if json["erro"]
+          Bridgetown.logger.warn "Liturgia API", "Erro na resposta: #{json['erro']}"
+          return nil
+        else
+          salvar_cache(json)
+          return json
+        end
+      else
+        Bridgetown.logger.error "Liturgia API", "Erro HTTP: #{response.code}"
+        return nil
+      end
     end
   rescue StandardError => e
     Bridgetown.logger.error "Liturgia API", "Exceção: #{e.message}"
-    nil
+    return nil
+  end
+
+  private
+
+  def self.cache_valido?
+    return false unless File.exist?(CACHE_FILE)
+
+    cache_time = File.mtime(CACHE_FILE)
+    cache_time > (Time.now - CACHE_EXPIRATION_DAYS * 86400)
+  end
+
+  def self.salvar_cache(data)
+    FileUtils.mkdir_p(File.dirname(CACHE_FILE))
+
+    File.write(CACHE_FILE, data.to_json)
   end
 end
 
